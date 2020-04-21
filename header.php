89405c82eb055d7ba82fd79312255b533bb74aed
gian <gpmantilla@gmail.com>
2020-04-21 01:31:28 -0300

    se separaron el hearder, el footer y se los llamos por requere_once. despues de comenzo a desparar el menu izquierdo de categoria, marca, y los proeductos  en clases diferentes para tener un mejor control a la hora programar por sectores.

---
+++ header.php
@@ -0,0 +1,53 @@
+    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Aroma Shop - Home</title>
+	<link rel="icon" href="img/Fevicon.png" type="image/png">
+  <link rel="stylesheet" href="vendors/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="vendors/fontawesome/css/all.min.css">
+	<link rel="stylesheet" href="vendors/themify-icons/themify-icons.css">
+  <link rel="stylesheet" href="vendors/nice-select/nice-select.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/owl.theme.default.min.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <!--================ Start Header Menu Area =================-->
+    <header class="header_area">
+        <div class="main_menu">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container">
+                    <a class="navbar-brand logo_h" href="index.php"><img src="img/logo.png" alt=""></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav menu_nav ml-auto mr-auto">
+                            <li class="nav-item active"><a class="nav-link" href="index.php">Home</a></li>
+                            <li class="nav-item submenu dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+                                    aria-haspopup="true" aria-expanded="false">Productos</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item"><a class="nav-link" href="category.php">Shop Category</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="single-product.php">Product
+                                            Details</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <!--================ End Header Menu Area =================-->
---
+++ left_category.php
@@ -0,0 +1,44 @@
+<?php
+require_once('class/classCategory.php');
+?>
+
+<div class="sidebar-categories">
+                    <div class="head">Categorias</div>
+                    <ul class="main-categories">
+                        <div class="panel-group category-products" id="accordian">
+                            <!--category-productsr-->
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <!--llamo a query categoria-->
+
+                                        <?php
+                                             $categoria =new sqlCategoria($con); 
+                                             foreach($categoria->getCategorias() as $row){ ?>
+                                        <ul href="shop.php?marca=&amp;categoria=<?php echo $row['id_categoria']?>">
+                                            <?php echo $row['nombre']?>
+                                            <div class="panel panel-default">
+                                                <div class="panel-heading">
+                                                    <h4 class="panel-title">
+                                                        <!--llamo a query categoria-->
+                                                        <?php
+                                                              $categoria =new sqlCategoria($con); 
+                                                              foreach($categoria->getCategorias($row['id_categoria']) as $row2){ ?>
+                                                        <li> <a
+                                                                href="shop.php?marca=&amp;categoria=<?php echo $row2['id_categoria']?>"><?php echo $row2['nombre']?></a>
+                                                        </li>
+                                                        <?php }?>
+                                                    </h4>
+                                                </div>
+                                            </div>
+                                            </lu>
+                                            <?php }?>
+                                    </h4>
+                                </div>
+                            </div>
+
+                            <!--fin de categoria-->
+                        </div>
+                    </ul>
+                </div>
